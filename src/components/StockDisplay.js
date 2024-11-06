04e661116f94ea54ea9caa959c0046df0606244b
nontawit <ntwitmn@gmail.com>
2024-11-07 01:40:50 +0700

    fix app

--- src/components/StockDisplay.js
+++ src/components/StockDisplay.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
+import '../App.css';
 
 function StockDisplay() {
   const [position, setPosition] = useState('');
